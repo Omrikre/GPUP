e3bdf62d12a495f605d7b887b04555bac1db6a8f
NoamRe <noamreches1@gmail.com>
2022-02-15 20:05:48 +0200

    login changes

--- src/login/loginServlet.java
+++
@@ -1,8 +0,0 @@
-package login;
-
-import jakarta.servlet.http.HttpServlet;
-
-public class loginServlet extends HttpServlet {
-    private final String SIGN_UP_URL = "../signup/signup.html";
-
-}
---
+++ src/utils/ServletUtils.java
@@ -0,0 +1,43 @@
+package utils;
+
+import Engine.users.UserManager;
+import jakarta.servlet.ServletContext;
+
+public class ServletUtils {
+    private static final String USER_MANAGER_ATTRIBUTE_NAME = "userManager";
+    private static final String CHAT_MANAGER_ATTRIBUTE_NAME = "chatManager";
+
+    private static final Object userManagerLock = new Object();
+    private static final Object chatManagerLock = new Object();
+
+    public static UserManager getUserManager(ServletContext servletContext) {
+
+        synchronized (userManagerLock) {
+            if (servletContext.getAttribute(USER_MANAGER_ATTRIBUTE_NAME) == null) {
+                servletContext.setAttribute(USER_MANAGER_ATTRIBUTE_NAME, new UserManager());
+            }
+        }
+        return (UserManager) servletContext.getAttribute(USER_MANAGER_ATTRIBUTE_NAME);
+    }
+
+    public static ChatManager getChatManager(ServletContext servletContext) {
+        synchronized (chatManagerLock) {
+            if (servletContext.getAttribute(CHAT_MANAGER_ATTRIBUTE_NAME) == null) {
+                servletContext.setAttribute(CHAT_MANAGER_ATTRIBUTE_NAME, new ChatManager());
+            }
+        }
+        return (ChatManager) servletContext.getAttribute(CHAT_MANAGER_ATTRIBUTE_NAME);
+    }
+
+    public static int getIntParameter(HttpServletRequest request, String name) {
+        String value = request.getParameter(name);
+        if (value != null) {
+            try {
+                return Integer.parseInt(value);
+            } catch (NumberFormatException numberFormatException) {
+            }
+        }
+        return INT_PARAMETER_ERROR;
+    }
+
+}
